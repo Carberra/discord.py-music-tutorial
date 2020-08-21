631cf378e8ed45aba8f0a8bbe9c141fa5ffa3031
parafoxia <parafoxia@carberra.xyz>
2020-08-21 13:42:10 +0100

    After episode 4

--- bot/cogs/music.py
+++ bot/cogs/music.py
@@ -1,11 +1,37 @@
+import typing as t
+
 import discord
 import wavelink
 from discord.ext import commands
 
 
+class AlreadyConnectedToChannel(commands.CommandError):
+    pass
+
+
+class NoVoiceChannel(commands.CommandError):
+    pass
+
+
 class Player(wavelink.Player):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+    async def connect(self, ctx, channel=None):
+        if self.is_connected:
+            raise AlreadyConnectedToChannel
+
+        if (channel := getattr(ctx.author.voice, "channel", channel)) is None:
+            raise NoVoiceChannel
+
+        await super().connect(channel.id)
+        return channel
+
+    async def teardown(self):
+        try:
+            await self.destroy()
+        except KeyError:
+            pass
 
 
 class Music(commands.Cog, wavelink.WavelinkMixin):
@@ -18,7 +44,7 @@ class Music(commands.Cog, wavelink.WavelinkMixin):
     async def on_voice_state_update(self, member, before, after):
         if not member.bot and after.channel is None:
             if not [m for m in before.channel.members if not m.bot]:
-                pass  # Disconnect the bot from channel. EP4
+                await self.get_player(member.guild).teardown()
 
     @wavelink.WavelinkMixin.listener()
     async def on_node_ready(self, node):
@@ -49,10 +75,29 @@ class Music(commands.Cog, wavelink.WavelinkMixin):
             await self.wavelink.initiate_node(**node)
 
     def get_player(self, obj):
-        if isinstance(obj, command.Context):
+        if isinstance(obj, commands.Context):
             return self.wavelink.get_player(obj.guild.id, cls=Player, context=obj)
         elif isinstance(obj, discord.Guild):
             return self.wavelink.get_player(obj.id, cls=Player)
+
+    @commands.command(name="connect", aliases=["join"])
+    async def connect_command(self, ctx, *, channel: t.Optional[discord.VoiceChannel]):
+        player = self.get_player(ctx)
+        channel = await player.connect(ctx, channel)
+        await ctx.send(f"Connected to {channel.name}.")
+
+    @connect_command.error
+    async def connect_command_error(self, ctx, exc):
+        if isinstance(exc, AlreadyConnectedToChannel):
+            await ctx.send("Already connected to a voice channel.")
+        elif isinstance(exc, NoVoiceChannel):
+            await ctx.send("No suitable voice channel was provided.")
+
+    @commands.command(name="disconnect", aliases=["leave"])
+    async def disconnect_command(self, ctx):
+        player = self.get_player(ctx)
+        await player.teardown()
+        await ctx.send("Disconnect.")
 
 
 def setup(bot):
